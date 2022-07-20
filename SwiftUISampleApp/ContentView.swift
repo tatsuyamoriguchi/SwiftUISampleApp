5ea7f5448ae8e99b646483a8418d700083614123
Brian Moriguchi <brian@beckos.com>
2022-07-20 15:37:42 -0700

    Initial Commit

---
+++ SwiftUISampleApp/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftUISampleApp
+//
+//  Created by Tatsuya Moriguchi on 7/20/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SwiftUISampleApp/SwiftUISampleAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUISampleAppApp.swift
+//  SwiftUISampleApp
+//
+//  Created by Tatsuya Moriguchi on 7/20/22.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUISampleAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
